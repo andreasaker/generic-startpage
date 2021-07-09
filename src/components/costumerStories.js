5ed95bf933a1b37b8c6bff51d4d8b9238a000f12
unknown <andreas.aker91@gmail.com>
2021-07-09 20:17:34 +0200

    Reviews continued

--- src/components/costumerStories.js
+++ src/components/costumerStories.js
@@ -5,15 +5,26 @@ import Review from "./review";
 
 const StyledWrapper = styled(Wrapper)`
   height: 35vh;
+  display:flex;
+  justify-content: center;
+`;
+
+const ReviewStack = styled.div`
+  display:flex;
   align-items: left;
   justify-content: left;
   flex-direction: row;
+  max-width: 1400px;
+  height:inherit; 
 `;
 
 const CostumerStories = () => {
   return (
     <StyledWrapper>
+      <ReviewStack>
       <Review />
+      <Review />
+      </ReviewStack>
     </StyledWrapper>
   );
 };
--- src/components/review.js
+++ src/components/review.js
@@ -2,13 +2,49 @@ import React from "react";
 import styled from "styled-components";
 
 const Card = styled.div`
-  width: 90vw;
-  height: inherit;
+  display: flex;
+  flex-direction: row;
+  width: 60em; 
+  height:inherit; 
   background-color: gray;
 `;
 
+const Img = styled.img`
+  height: 100%;
+`;
+
+const Box = styled.div`
+display: flex;
+flex-direction: column;
+padding: 2em 2em 2em 2em;
+font-size: 28px;
+width:45%;
+`;
+
+const P = styled.p`
+font-size: 38px;
+font-weight: 700;
+`;
+
+const Canvas = styled.div`
+overflow: hidden;
+width:55%;
+height: 100%;
+`;
+
 const Review = (text, subtext, picture) => {
-  return <Card>"QUESTADAWDWAD DAWDAWD DWADAWDAW"</Card>;
+  return (
+  <Card>
+  <Box>
+  <P>"Our dog always come home with a smile."</P>
+  <div>Melone & Tim</div>
+  </Box>
+  
+  <Canvas>
+  <Img src="happy-corgi.jpg" alt="BIld av huoadg5888 från Pixabay"/>
+  </Canvas>
+  
+  </Card>);
 };
 
 export default Review;
