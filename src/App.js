4accc5eaeca025fc922020e928e53b309409a676
unknown <andreas.aker91@gmail.com>
2021-09-15 21:42:12 +0200

    first api calls made

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
 import StartPage from "./pages/startPage";
 import { data } from "./fakeData";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [apiData, setApiData] = useState([]);
+  const [token, setToken] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_USERNAME);
+    if (token === null) {
+      axios
+        .post(`${process.env.REACT_APP_API}/auth/local`, {
+          identifier: process.env.REACT_APP_USERNAME,
+          password: process.env.REACT_APP_PASSWORD,
+        })
+        .then((resp) => setToken(resp.data.jwt))
+        .catch((err) => console.log(err));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (token !== null) {
+      axios
+        .get("http://localhost:1337/pages", {
+          headers: {
+            Authorization: "Bearer " + token,
+          },
+        })
+        .then((resp) => setApiData(resp));
+    }
+  }, [token]);
+
+  console.log(apiData.data[0]);
   return (
     <div>
       <StartPage data={data} />
